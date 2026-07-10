79f641c5599e61573ffbbad5c6df124563f33037
ulas-kaplan <ulas.kaplan.1.2.3@gmail.com>
2026-07-10 11:05:57 +0000

    kernel coding styles applied

--- main.c
+++ main.c
@@ -11,144 +11,144 @@
 #include <getopt.h>
 
 /*
-  global variables for state.
+ states modified by signal handlers.
  */
 volatile int g_run = 1;
 volatile int g_reload = 0;
 
-/* log variables */
+/* configuration */
 int g_level = 1;
 int g_syslog = 0;
 FILE *g_file = NULL;
 
-/*
-  simple signal handler.
-  just sets the flags --so main loop can do the work.
- */
 static void handle_sig(int sig)
 {
-        if (sig == SIGTERM || sig == SIGINT)
-                g_run = 0;
-        else if (sig == SIGHUP)
-                g_reload = 1;
+	if (sig == SIGTERM || sig == SIGINT)
+		g_run = 0;
+	else if (sig == SIGHUP)
+		g_reload = 1;
+}
+
+static void do_log(int level, const char *msg)
+{
+	if (level > g_level)
+		return;
+
+	if (g_syslog) {
+		int sys_lvl = LOG_INFO;
+
+		if (level == 0)
+			sys_lvl = LOG_ERR;
+		else if (level == 2)
+			sys_lvl = LOG_DEBUG;
+		
+		syslog(sys_lvl, "%s", msg);
+	} else if (g_file) {
+		fprintf(g_file, "%s", msg);
+		fflush(g_file);
+	}
+}
+
+static int daemonize(void)
+{
+	pid_t pid;
+	int i;
+	int fd;
+
+	pid = fork();
+	if (pid < 0)
+		return -1;
+	if (pid > 0)
+		exit(EXIT_SUCCESS);
+
+	if (setsid() < 0)
+		return -1;
+
+	pid = fork();
+	if (pid < 0)
+		return -1;
+	if (pid > 0)
+		exit(EXIT_SUCCESS);
+
+	umask(0);
+
+	if (chdir("/") < 0)
+		return -1;
+
+	for (i = 0; i < 1024; i++) {
+		if (g_file && i == fileno(g_file))
+			continue;
+		close(i);
+	}
+
+	fd = open("/dev/null", O_RDWR);
+	if (fd >= 0) {
+		dup2(fd, STDIN_FILENO);
+		dup2(fd, STDOUT_FILENO);
+		dup2(fd, STDERR_FILENO);
+		if (fd > 2)
+			close(fd);
+	}
+
+	return 0;
 }
 
 int main(int argc, char *argv[])
 {
-        int opt;
-        int i;
-        int fd;
-        pid_t pid;
-        struct sigaction sa;
+	struct sigaction sa;
+	int opt;
 
-        /* Parse command line arguments */
-        while ((opt = getopt(argc, argv, "o:l:")) != -1) {
-                if (opt == 'o') {
-                        if (strcmp(optarg, "syslog") == 0) {
-                                g_syslog = 1;
-                        } else {
-                                g_file = fopen(optarg, "a");
-                                if (!g_file)
-                                        goto err_out;
-                        }
-                } else if (opt == 'l') {
-                        g_level = atoi(optarg);
-                }
-        }
+	/* parse arguments -o and -l */
+	while ((opt = getopt(argc, argv, "o:l:")) != -1) {
+		if (opt == 'o') {
+			if (strcmp(optarg, "syslog") == 0) {
+				g_syslog = 1;
+			} else {
+				g_file = fopen(optarg, "a");
+				if (!g_file)
+					return EXIT_FAILURE;
+			}
+		} else if (opt == 'l') {
+			g_level = atoi(optarg);
+		}
+	}
 
-        /*
-          daemonize process.
-          doing it inline here 
-         */
-        pid = fork();
-        if (pid < 0)
-                goto err_out;
-        if (pid > 0)
-                exit(0);
+	/* become a daemon */
+	if (daemonize() < 0)
+		return EXIT_FAILURE;
 
-        if (setsid() < 0)
-                goto err_out;
+	/* setup signal handling */
+	sa.sa_handler = handle_sig;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
 
-        pid = fork();
-        if (pid < 0)
-                goto err_out;
-        if (pid > 0)
-                exit(0);
+	sigaction(SIGTERM, &sa, NULL);
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGHUP, &sa, NULL);
 
-        umask(0);
+	if (g_syslog)
+		openlog("mydaemon", LOG_PID, LOG_DAEMON);
 
-        if (chdir("/") < 0)
-                goto err_out;
+	do_log(1, "[INFO] Daemon started.\n");
 
-        /* close all open files except log file */
-        for (i = 0; i < 1024; i++) {
-                if (g_file && i == fileno(g_file))
-                        continue;
-                close(i);
-        }
+	/* main background loop */
+	while (g_run) {
+		if (g_reload) {
+			do_log(1, "[INFO] Reloading configuration - SIGHUP received.\n");
+			g_reload = 0;
+		}
 
-        /* redirect stdin - stdout - stderr to ===>  /dev/null */
-        fd = open("/dev/null", O_RDWR);
-        if (fd >= 0) {
-                dup2(fd, 0);
-                dup2(fd, 1);
-                dup2(fd, 2);
-                if (fd > 2)
-                        close(fd);
-        }
+		sleep(5);
+		do_log(2, "[DEBUG] Daemon is running.\n");
+	}
 
-        /* setup signals */
-        sa.sa_handler = handle_sig;
-        sigemptyset(&sa.sa_mask);
-        sa.sa_flags = 0;
-        
-        sigaction(SIGTERM, &sa, NULL);
-        sigaction(SIGINT,  &sa, NULL);
-        sigaction(SIGHUP,  &sa, NULL);
+	/* shutdown */
+	do_log(1, "[INFO] Shutdown signal received. Daemon exiting.\n");
 
-        if (g_syslog)
-                openlog("my_daemon", LOG_PID, LOG_DAEMON);
+	if (g_syslog)
+		closelog();
+	if (g_file)
+		fclose(g_file);
 
-        /*
-         * main background loop
-         */
-        while (g_run) {
-                if (g_reload) {
-                        if (g_syslog && g_level >= 1) {
-                                syslog(LOG_INFO, "Reloading config...\n");
-                        } else if (g_file && g_level >= 1) {
-                                fprintf(g_file, "Reloading config...\n");
-                                fflush(g_file);
-                        }
-                        g_reload = 0;
-                }
-
-                sleep(5);
-
-                if (g_syslog && g_level >= 2) {
-                        syslog(LOG_DEBUG, "Daemon is running...\n");
-                } else if (g_file && g_level >= 2) {
-                        fprintf(g_file, "Daemon is running...\n");
-                        fflush(g_file);
-                }
-        }
-
-        /* cleanup */
-        if (g_syslog && g_level >= 1)
-                syslog(LOG_INFO, "Daemon exiting...\n");
-        else if (g_file && g_level >= 1) {
-                fprintf(g_file, "Daemon exiting...\n");
-                fflush(g_file);
-        }
-
-        if (g_syslog)
-                closelog();
-        if (g_file)
-                fclose(g_file);
-
-        return 0;
-
-/* error exit */
-err_out:
-        return 1;
+	return EXIT_SUCCESS;
 }
